5de33fcd14f411fe335f8deb3772b81ec7ac9547
Eraine Otayde <eraine.otayde@gmail.com>
2020-09-11 01:45:33 +0800

    Create project skeleton

---
+++ README.md
@@ -0,0 +1,8 @@
+# Vanilla Webservices
+
+A simple maven project demonstrating how to create and consume SOAP and REST web services without using external
+ libraries.
+
+## Getting Started
+
+Still WIP but we will get there.
---
+++ rest-client/src/main/java/ph/eraine/poc/webservices/restclient/Application.java
@@ -0,0 +1,12 @@
+package ph.eraine.poc.webservices.restclient;
+
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+public class Application {
+
+    public static void main(String[] args) {
+        log.info("we'll do something here soon.");
+    }
+
+}
